e79de33318ed8bc832aca8ce119781a195cf12bd
LeonidPanov <leonardopanov@yahoo.com>
2022-02-17 02:20:48 +0200

    Hm1

--- lesson1/App.js
+++ lesson1/App.js
@@ -12,23 +12,25 @@
 //
 // const fs = require('fs');
 // const path = require('path');
+const fs = ('fs');
+const path = ('path');
 
-// fs.mkdir(path.join(__dirname, 'Hm', 'main', 'inPerson'), {recursive: true}, (err) => {
-//     if (err) {
-//         console.log(err);
-//         throw err
-//     }
-//     fs.mkdir(path.join(__dirname, 'Hm', 'main' ,'online'),(err1 => {
-//         if (err1){
-//             console.log(err1);
-//             throw err1
-//         }
-//     }))
-// });
+fs.mkdir(path.join(__dirname, 'Hm', 'main', 'inPerson'), {recursive: true}, (err) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    fs.mkdir(path.join(__dirname, 'Hm', 'main', 'online'), (err1 => {
+        if (err1) {
+            console.log(err1);
+            throw err1
+        }
+    }));
+});
 
-// let one = [{name: "Andrii", age: 22, city: "Lviv"}];
-// let two = [{name: "Zara", age: 25, city: "Praga"}]
-//
+let one = [{name: "Andrii", age: 22, city: "Lviv"}];
+let two = [{name: "Zara", age: 25, city: "Praga"}]
+
 
 // for (const i of one) {
 //     for (const e in i)
@@ -83,73 +85,4 @@
 // changeFiles()
 
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Classwork<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-// 1 1. Спробуйте створити якийсь файл txt, прочитайте з нього дані і
-// одразу, дані які ви отримали запишіть їх в інший файл, в вас вийде невеликий callback hell, пізніше я вам покажу
-// // як можна це обійти, але поки зробіть так
-
-
-// const fs = require('fs');
-// const path = require('path');
-
-// fs.mkdir(path.join(__dirname, 'Cw'), (err => {
-//     if (err) {
-//         console.log(err);
-//         throw err
-//     }
-// }));
-
-// fs.writeFile(path.join(__dirname, 'Cw', 'text.txt'), 'some info', (err => {
-//     if (err) {
-//         console.log(err);
-//         throw err
-//     }
-//     fs.readFile(path.join(__dirname, 'Cw', 'text.txt'), 'utf8', (err1, data) => {
-//         if (err1) {
-//             console.log(err1);
-//             throw err1
-//         }
-//         fs.writeFile(path.join(__dirname, 'Cw', 'text2.txt'), `${data}`, (err2 => {
-//             if (err2) {
-//                 console.log(err2);
-//                 throw err2;
-//             }
-//         }))
-//     });
-// }));
-
-// 2.2. Створіть файл ( можете вручну ) заповніть його якимись даними
-// Прочитайте його, скопіюйте всі дані з нього
-// і перенесіть їх в нову папку та файл в ній, старий файл видаліть після того як все завершиться. Також вийде callback hell
-//
-// fs.writeFile(path.join(__dirname, 'Cw', 'file.txt'), 'some info', (err => {
-//     if (err) {
-//         console.log(err);
-//         throw err
-//     }
-//     fs.readFile(path.join(__dirname, 'Cw', 'file.txt'), 'utf8', (err1, data) => {
-//         if (err1) {
-//             console.log(err1);
-//             throw (err1);
-//         }
-//         fs.mkdir(path.join(__dirname, 'Cw', 'newFile'), (err2 => {
-//             if (err2) {
-//                 console.log(err2);
-//                 throw err2
-//             }
-//             fs.writeFile(path.join(__dirname, 'Cw', 'newFile', 'new.txt'), data, (err3 => {
-//                 if (err3) {
-//                     console.log(err3);
-//                     throw err3;
-//                 }
-//                 fs.unlink(path.join(__dirname, 'Cw', 'file.txt'), (err4 => {
-//                     if (err4) {
-//                         console.log(err4);
-//                         throw err4
-//                     }
-//                 }));
-//             }));
-//         }));
-//     })
-//
-// }));
+/
